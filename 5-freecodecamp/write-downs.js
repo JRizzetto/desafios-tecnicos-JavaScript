3f93a3cd6b7ced1d599a8cf8d6ad7c18983808fa
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-11-27 11:51:15 -0300

    freecodecamp -> Tasks

---
+++ 5-freecodecamp/write-downs.js
@@ -0,0 +1,60 @@
+// // Symbol: The Symbol data type is a unique and immutable value that may be used as an identifier for object properties.
+
+// const cryptickey1 = Symbol("saltNpepper");
+// const cryptickey2 = Symbol("saltNpepper");
+// console.log(cryptickey1 === cryptickey2); // false
+
+// // BigInt: When the number is too large for the Number data type, you can use the BigInt data type to represent integers of arbitrary length.
+// // By adding an n to the end of the number, you can create a BigInt.
+
+// const veryBigNumber = 1234567890123456789012345678901234567890n;
+// console.log(veryBigNumber);
+
+// // Strings are immutable in JavaScript. This means that once a string is created, you cannot change the characters in the string. However, you can still reassign strings to
+// // a new value.
+
+// let testeString = "Jefferson";
+// let keyOn = false;
+
+// if (!keyOn) {
+//   testeString = "Mario";
+// }
+
+// console.log(testeString);
+
+// // Another way you can concatenate strings is to use the concat() method. This method joins two or more strings together.
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullname = firstName.concat(" ", lastName);
+// console.log(fullname);
+
+// let hello = "Hello";
+// hello += " world";
+// console.log(hello);
+
+// let statement = 'She said, "Hello!"';
+// console.log(statement); // She said, "Hello!"
+
+// let quote = "It's a beautiful day";
+// console.log(quote);
+
+// Here is an example of using the indexOf() method to find the position for the string awesome:
+
+// let sentence = "JavaScript is awesome!";
+// let position = sentence.indexOf("awesome!");
+// console.log(position);
+
+// let sentence1 = "JavaScript is awesome!";
+// let position1 = sentence1.indexOf("fantastic");
+// console.log(position1);
+
+// let sentence2 = "JavaScript is awesome, and JavaScript is powerful!";
+// let position2 = sentence2.indexOf("JavaScript", 15);
+// console.log(position2);
+
+// console.log("freeCodeCamp".indexOf("F"));
+// console.log("freeCodeCamp".indexOf("f"));
+
+
+
