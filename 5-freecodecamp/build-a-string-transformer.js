1a4c11cf48cf49a72c1989ddd68b15562b91236b
JRizzetto <jeffersonrizzetto@gmail.com>
2025-12-30 19:03:24 -0300

    build-a-string-transformer

---
+++ 5-freecodecamp/build-a-string-transformer.js
@@ -0,0 +1,6 @@
+/* Step 1
+In this workshop, you will practice working with the replace(), replaceAll() and repeat() methods by building a string transformer app.
+To start, create a variable called originalString and assign it the value of "I love cats.".
+*/
+
+let originalString = "I love cats.";
