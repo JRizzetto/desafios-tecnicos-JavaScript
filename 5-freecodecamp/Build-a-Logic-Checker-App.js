f5761e2405288c4538e6da8a9a426a10ac0cc131
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-22 19:35:07 -0300

    Responsive Web Design Certification

--- 5-freecodecamp/Build-a-Logic-Checker-App.js
+++ 5-freecodecamp/Build-a-Logic-Checker-App.js
@@ -87,13 +87,74 @@ if (hasDeveloperJob) {
 }
 
 /*
-
+Step 8
+Declare another variable named isTimmyAGamer and assign it the boolean false.
 */
+let isTimmyAGamer = false;
 
 /*
+Step 9
+Now create another if statement that uses isTimmyAGamer as its condition. Inside the body of your if statement, log "Timmy loves to play World of Warcraft." to the console.
 
+Note that you won't see anything logged to the console as the condition of your new if statement is false.
 */
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
 
+/*
+Step 10
+Declare another variable named timmyAge and set it to 18.
+*/
+const timmyAge = 18;
+
+/*
+Step 11
+Comparison operators allow you to compare values and write more complex conditional statements.
+
+Example Code
+if (today == birthday) {
+  console.log("Happy Birthday!");
+}
+For example, the code above logs "Happy Birthday!" to the console only when the values of the variables today and birthday are equal.
+
+Create a third if statement. For its condition, use the >= operator to check if timmyAge is greater than or equal to 16.
+
+Inside the body of your new if statement, log "Timmy is old enough to drive." to the console.
+*/
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+}
+
+/*
+Step 12
+A conditional statement can have an else clause, which runs code when the if condition is falsy. Here's an example of an if...else statement:
+
+Example Code
+if (condition) {
+  console.log("condition is truthy");
+} else {
+  console.log("condition is falsy");
+}
+Add an else clause to the existing if statement. Inside the body of your else clause, log "Timmy is not old enough to drive." to the console.
+*/
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
+
+/*
+Step 13
+Finally, change the declaration of timmyAge and set it to a number less than 16. After that you'll see "Timmy is not old enough to drive." logged to the console.
+*/
+timmyAge = 15;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
 /*
 
 */
